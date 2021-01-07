7c9ff661d97a7ba6a9a29541e060325ec2f6348c
affanhanifathtarech <affanhanifathtarech@gmail.com>
2021-01-07 07:53:29 +0700

    whatsapp-api

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const { Client } = require('whatsapp-web.js');
+const { Client, MessageMedia } = require('whatsapp-web.js');
 const express = require('express');
 const { body, validationResult } = require('express-validator');
 const qrcode = require('qrcode');
 const socketIO = require('socket.io');
 const http = require('http');
 const fs = require('fs');
+const { phoneNumberFormatter } = require('./helpers/formatter');
+const fileUpload = require('express-fileupload');
+const axios = require('axios');
 
 const app = express();
 const server = http.createServer(app);
@@ -12,6 +15,8 @@ const io = socketIO(server);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({ debug: true }));
+
 const SESSION_FILE_PATH = './whatsapp-session.json';
 let sessionCfg;
 if (fs.existsSync(SESSION_FILE_PATH)) {
@@ -22,11 +27,25 @@ app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname });
 });
 
-const client = new Client({ puppeteer: { headless: true }, session: sessionCfg });
+const client = new Client({
+    restartOnAuthFail: true,
+    puppeteer: {
+      headless: true,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process', // <- this one doesn't works in Windows
+        '--disable-gpu'
+      ],
+    },
+    session: sessionCfg
+  });
 // You can use an existing session and avoid scanning a QR code by adding a "session" object to the client options.
 // This object must include WABrowserId, WASecretBundle, WAToken1 and WAToken2.
-
-
 
 client.on('message', msg => {
     if (msg.body == 'halo') {
@@ -42,49 +61,56 @@ client.on('message', msg => {
 
 client.initialize();
 
-//socketIO
-io.on('connection', function(socket){
-    socket.emit('message', 'connecting...');
-    
+// Socket IO
+io.on('connection', function(socket) {
+    socket.emit('message', 'Connecting...');
+  
     client.on('qr', (qr) => {
-        // Generate and scan this code with your phone
-        console.log('QR RECEIVED', qr);
-        qrcode.toDataURL(qr, (err, url) => {
-            socket.emit('qr', url);
-            socket.emit('message', 'QR code diterima, silakan scan!');
-        });
+      console.log('QR RECEIVED', qr);
+      qrcode.toDataURL(qr, (err, url) => {
+        socket.emit('qr', url);
+        socket.emit('message', 'QR Code received, scan please!');
+      });
     });
-    
-    client.on('auth_failure', msg => {
-        // Fired if session restore was unsuccessfull
-        console.error('AUTHENTICATION FAILURE', msg);
-        socket.emit('message', 'Gagal! Autentikasi Error!');
-    });
-
+  
     client.on('ready', () => {
-        socket.emit('ready', 'Whatsapp terkoneksi! Anda telah siap!');
-        socket.emit('message', 'Whatsapp terkoneksi! Anda telah siap!');
+      socket.emit('ready', 'Whatsapp is ready!');
+      socket.emit('message', 'Whatsapp is ready!');
     });
-    
+  
     client.on('authenticated', (session) => {
-        socket.emit('authenticated', 'Terautentikasi!');
-        socket.emit('message', 'Terautentikasi!');
-        console.log('AUTHENTICATED', session);
-        sessionCfg=session;
-        fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
-            if (err) {
-                console.error(err);
-            }
-        });
+      socket.emit('authenticated', 'Whatsapp is authenticated!');
+      socket.emit('message', 'Whatsapp is authenticated!');
+      console.log('AUTHENTICATED', session);
+      sessionCfg = session;
+      fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function(err) {
+        if (err) {
+          console.error(err);
+        }
+      });
     });
-});
+  
+    client.on('auth_failure', function(session) {
+      socket.emit('message', 'Auth failure, restarting...');
+    });
+  
+    client.on('disconnected', (reason) => {
+      socket.emit('message', 'Whatsapp is disconnected!');
+      fs.unlinkSync(SESSION_FILE_PATH, function(err) {
+          if(err) return console.log(err);
+          console.log('Session file deleted!');
+      });
+      client.destroy();
+      client.initialize();
+    });
+  });
+  
+const checkRegisteredNumber = async function(number) {
+    const isRegistered = await client.isRegisteredUser(number);
+    return isRegistered;
+  }
 
-client.on('disconnected', (reason) => {
-    console.log('Client was logged out', reason);
-    socket.emit('message', 'Terputus!');
-});
-
-//POST POINT
+//SEND MEDIA
 app.post('/send-message', [
     body('number').notEmpty(),
     body('message').notEmpty()
@@ -99,8 +125,18 @@ app.post('/send-message', [
             message: errors.mapped()
         });
     }
-    const number = req.body.number;
+    const number = phoneNumberFormatter(req.body.number);
     const message = req.body.message;
+
+    const isRegisteredNumber = await checkRegisteredNumber(number);
+
+    if (!isRegisteredNumber) {
+      return res.status(422).json({
+        status: false,
+        message: 'The number is not registered'
+      });
+    }
+
     client.sendMessage(number, message).then(response => {
         res.status(200).json({
             status: true,
@@ -113,6 +149,43 @@ app.post('/send-message', [
         });
     });
 });
+
+// Send media
+app.post('/send-media', async (req, res) => {
+    const number = phoneNumberFormatter(req.body.number);
+    const caption = req.body.caption;
+
+    /* --- kirim gambar dari path local --- */
+    // const media = MessageMedia.fromFilePath('./send.png'); 
+   
+    
+    /* --- kirim gambar dari file upload --- */
+    // const file = req.files.file;
+    // const media = new MessageMedia(file.mimetype, file.data.toString('base64'), file.name);
+    
+    /* --- kirim gambar dari url --- */
+    const fileUrl = req.body.file;
+    let mimetype;
+    const attachment = await axios.get(fileUrl, {
+      responseType: 'arraybuffer'
+    }).then(response => {
+      mimetype = response.headers['content-type'];
+      return response.data.toString('base64');
+    });
+    const media = new MessageMedia(mimetype, attachment, 'Media');
+  
+    client.sendMessage(number, media, { caption: caption }).then(response => {
+      res.status(200).json({
+        status: true,
+        response: response
+      });
+    }).catch(err => {
+      res.status(500).json({
+        status: false,
+        response: err
+      });
+    });
+  });
 
 server.listen(8000, function() {
     console.log('app running port:' + 8000);
